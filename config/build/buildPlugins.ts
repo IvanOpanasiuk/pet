0cb7a1289a0249395bc6ca8c0340eda24c4f2c13
Iva Opanasiuk <vanjoker88@gmail.com>
2023-02-22 14:02:08 +0400

    added bundle-analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,8 @@ import { type BuildOptions } from './types/config';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
 import webpack from 'webpack';
 
 export function buildPlugins ({
@@ -18,6 +20,9 @@ export function buildPlugins ({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
     })
   ];
 
