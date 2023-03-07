71110d43ae7c28940ed5af307026e91b6dd4eff5
Iva Opanasiuk <vanjoker88@gmail.com>
2023-03-07 18:02:54 +0400

    fixed App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,21 @@
+
 import { useTheme } from 'app/providers/ThemeProvider';
 
 import classNames from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 
 const App = (): JSX.Element => {
   const { theme } = useTheme();
+
+  useEffect(() => {
+    document.body.className = theme;
+    return () => {
+      document.body.classList.remove(theme);
+    };
+  }, [theme]);
 
   return (
       <div className={classNames('app', {}, [theme])}>
