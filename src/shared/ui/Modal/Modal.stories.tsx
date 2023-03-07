5b37e68ffa75ebeb16737c6c344a04977d8a3ce3
Iva Opanasiuk <vanjoker88@gmail.com>
2023-03-07 19:06:25 +0400

    added mock server

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -1,8 +1,8 @@
 import { type ComponentStory, type ComponentMeta } from '@storybook/react';
 
 import { Modal } from './Modal';
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { Theme } from 'app/providers/ThemeProvider';
+// import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+// import { Theme } from 'app/providers/ThemeProvider';
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 export default {
@@ -21,10 +21,10 @@ Normal.args = {
   children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Veniam ipsam facere, modi itaque at perferendis.Amet saepe consectetur, repudiandae tenetur porro expedita,illo, excepturi ipsum nostrum deleniti adipisci cumque quibusdam.'
 };
 
-export const Dark = Template.bind({});
-Dark.args = {
-  isOpen: true,
-  children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Veniam ipsam facere, modi itaque at perferendis.Amet saepe consectetur, repudiandae tenetur porro expedita,illo, excepturi ipsum nostrum deleniti adipisci cumque quibusdam.'
-};
+// export const Dark = Template.bind({});
+// Dark.args = {
+//   isOpen: true,
+//   children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Veniam ipsam facere, modi itaque at perferendis.Amet saepe consectetur, repudiandae tenetur porro expedita,illo, excepturi ipsum nostrum deleniti adipisci cumque quibusdam.'
+// };
 
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+// Dark.decorators = [ThemeDecorator(Theme.DARK)];
